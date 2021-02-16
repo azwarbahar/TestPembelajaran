45ddf1e7f971e56b9a43384905096cb95b4b6b87
azwarbahar07 <azwarbahar07@gmail.com>
2021-02-16 17:59:47 +0800

    Initial commit

---
+++ app/src/main/java/com/pembelajaran/testpembelajaran/SpalshScreenActivity.java
@@ -0,0 +1,14 @@
+package com.pembelajaran.testpembelajaran;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SpalshScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spalsh_screen);
+    }
+}
